745fda6fcb6494b1fbfe2b3124e20a37269052ec
Michael Wanush <mike@wanush.net>
2019-05-09 21:57:39 +0200

    Adding Bootstrap
    Added Bootstrap to the mix

--- src/App/App.js
+++ src/App/App.js
@@ -1,13 +1,20 @@
-import React from 'react'
+import React from "react"
+import Row from "react-bootstrap/Row"
+import Col from "react-bootstrap/Col"
+import Container from "react-bootstrap/Container"
 
-import '../styles/App.scss'
+import "../styles/App.scss"
 
 class App extends React.Component {
   render() {
     return (
-      <div>
-        <h1>My React App!</h1>
-      </div>
+      <Container>
+        <Row>
+          <Col lg={{span: 4, offset: 4}}>
+            <h1>My React App!</h1>
+          </Col>
+        </Row>
+      </Container>
     )
   }
 }
--- src/index.js
+++ src/index.js
@@ -3,3 +3,4 @@ import ReactDOM from 'react-dom'
 import App from './App/App.js'
 
 ReactDOM.render(<App />, document.getElementById('root'))
+
