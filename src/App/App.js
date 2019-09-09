88777a82c7b60b16c7cc213a1a33c0d58e482678
Michael Wanush <mike@wanush.net>
2019-09-09 22:05:22 +0200

    updates

--- src/App/App.js
+++ src/App/App.js
@@ -1,22 +1,22 @@
-import React from "react"
-import Row from "react-bootstrap/Row"
-import Col from "react-bootstrap/Col"
-import Container from "react-bootstrap/Container"
+import React from "react";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import Container from "react-bootstrap/Container";
 
-import "../styles/App.scss"
+import "../styles/App.scss";
 
 class App extends React.Component {
-  render() {
-    return (
-      <Container>
-        <Row>
-          <Col lg={{span: 4, offset: 4}}>
-            <h1>My React App!</h1>
-          </Col>
-        </Row>
-      </Container>
-    )
-  }
+    render() {
+        return (
+            <Container>
+                <Row>
+                    <Col lg={{span: 4, offset: 4}}>
+                        <h1>My React App!</h1>
+                    </Col>
+                </Row>
+            </Container>
+        );
+    }
 }
 
-export default App
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import App from './App/App.js'
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App/App.js";
 
-ReactDOM.render(<App />, document.getElementById('root'))
-
+ReactDOM.render(<App/>, document.getElementById("root"));
